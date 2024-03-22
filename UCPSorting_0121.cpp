251ceabdf3cc00a7932bb2c5f72b8a3303a48844
Lenovo <9.3razzinayyuri@gmail.com>
2024-03-22 10:16:42 +0700

    Add project files.

---
+++ UCPSorting_0121.cpp
@@ -0,0 +1,41 @@
+//Soal tipe A
+//1.Jelaskan bagaimana insertsionSort membandingkan serta menukar elemen-elemen?
+  //InsertionSort membandingkan dengan cara membagi array menjadi 2 bagian yang belum tersortir dan yang sudah tersortir
+  //Menyortir array satu persatu, memastiakan elemen sudah benar 
+  //Jika telah tersortir, masukan ke dalam bagian yang telah disortir
+  //Melakukan pembandingan array dari yang terkecil ke yang besar 
+//2.Jelaskan bagaimana SelectionSort membandingkan serta menukar elemen-elemen?
+  //SelectionSort membandingkan dengan cara mengurutkan array dr yang terbesar ke yang kecil
+//3.Dalam algoritma sortir, bagaimana cara untuk mengetahui jumlah langkah yang dilakukan? jelaskan.
+  //Menentukan jumlah array dan mengitung penyortiran yang telah dilakukan variabel J 
+
+#include <iostream>
+using namespace std;
+
+int razzin[21];
+int n;
+
+void input() {
+    while (true) {
+        cout << "Masukan banyak elemen pada array : ";
+        cin >> n;
+        if (n <= 21)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal elemen 21 elemen.\n";
+
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "=====================" << endl;
+
+    for (int)
+
+}
+int main()
+{
+    
+}
+
